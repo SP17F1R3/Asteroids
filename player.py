e9ce4c969d215c2a1cf0aa509d161e1be7f45c0e
SP17F1R3 <altair.th3.eagle@gmail.com>
2025-09-16 11:19:49 -0400

    E: corrected player.py added project json

--- player.py
+++ player.py
@@ -5,8 +5,7 @@ from constants import PLAYER_RADIUS
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-
-    rotation = 0
+        self.rotation = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
